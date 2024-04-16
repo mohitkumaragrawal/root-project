657cac5bc704fc6f71c3f5c1bdc2f0320929e8dd
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2024-04-16 22:56:47 +0530

    icon

--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </ContactPopupContextProvider>
     </FirebaseProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
